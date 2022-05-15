6e248e3dfa40a19194da15c64dbb445e21b926f5
langvs <55701615+langvs@users.noreply.github.com>
2022-05-15 22:01:55 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="*.beib.cc";
+        url.hostname="daohang.beib.cc";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
